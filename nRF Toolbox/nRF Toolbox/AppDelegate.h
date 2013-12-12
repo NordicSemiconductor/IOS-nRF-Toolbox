647f36332844925222d7774af5d51c32a0fca179
alno <alno@94a72e49-5737-764a-8bf3-0d5ceb61e552>
2013-12-12 13:34:03 +0000

    Commiting the initial source code of the application.

---
+++ nRF Toolbox/nRF Toolbox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  nRF Toolbox
+//
+//  Created by Aleksander Nowakowski on 12/12/13.
+//  Copyright (c) 2013 Nordic Semiconductor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ nRF Toolbox/nRF Toolbox/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  nRF Toolbox
+//
+//  Created by Aleksander Nowakowski on 12/12/13.
+//  Copyright (c) 2013 Nordic Semiconductor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
