12b1084a6f45a97f319bcf5c2b7803202ad22181
alno <alno@94a72e49-5737-764a-8bf3-0d5ceb61e552>
2014-01-09 11:30:59 +0000

    Some more common code to use

--- nRF Toolbox/HTSViewController.m
+++ nRF Toolbox/HTSViewController.m
@@ -72,6 +72,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)appDidEnterBackground:(NSNotification *)_notification
+{
+    //    UILocalNotification *notification = [[UILocalNotification alloc]init];
+    //    notification.alertAction = @"Show";
+    //    notification.alertBody = @"You are still connected to Running Speed and Cadence sensor. It will collect data also in background.";
+    //    notification.hasAction = NO;
+    //    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:1];
+    //    notification.timeZone = [NSTimeZone  defaultTimeZone];
+    //    notification.soundName = UILocalNotificationDefaultSoundName;
+    //    [[UIApplication sharedApplication] setScheduledLocalNotifications:[NSArray arrayWithObject:notification]];
+}
+
+-(void)appDidBecomeActiveBackground:(NSNotification *)_notification
+{
+    //    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+}
+
 - (IBAction)connectOrDisconnectClicked {
     if (connectedPeripheral != nil)
     {
@@ -123,5 +140,53 @@
         NSLog(@"Bluetooth not ON");
     }
 }
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral
+{
+    // Scanner uses other queue to send events. We must edit UI in the main queue
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [deviceName setText:peripheral.name];
+        [connectButton setTitle:@"DISCONNECT" forState:UIControlStateNormal];
+    });
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActiveBackground:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    
+    // Peripheral has connected. Discover required services
+    connectedPeripheral = peripheral;
+    [peripheral discoverServices:@[htsServiceUUID, batteryServiceUUID]];
+}
+
+-(void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    // Scanner uses other queue to send events. We must edit UI in the main queue
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Connecting to the peripheral failed. Try again" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [alert show];
+        [connectButton setTitle:@"CONNECT" forState:UIControlStateNormal];
+        connectedPeripheral = nil;
+        
+        [self clearUI];
+    });
+}
+
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    // Scanner uses other queue to send events. We must edit UI in the main queue
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [connectButton setTitle:@"CONNECT" forState:UIControlStateNormal];
+        connectedPeripheral = nil;
+        
+        [self clearUI];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
+    });
+}
+
+- (void) clearUI
+{
+    // TODO
+}
+
 
 @end
