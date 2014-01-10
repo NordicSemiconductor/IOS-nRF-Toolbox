97023d22a15e5634ffb92384fc8c8420395be3c3
alno <alno@94a72e49-5737-764a-8bf3-0d5ceb61e552>
2014-01-10 14:04:50 +0000

    4 new View controllers added for missing profiles: BGM, BPM, DFU, Proximity.

--- nRF Toolbox/Constants.h
+++ nRF Toolbox/Constants.h
@@ -11,6 +11,18 @@
 
 #define is4InchesIPhone ([[UIScreen mainScreen] bounds].size.height == 568)
 
+static NSString * const bgmServiceUUIDString = @"00001808-0000-1000-8000-00805F9B34FB";
+static NSString * const bgmGlucoseMeasurementCharacteristicUUIDString = @"00002A18-0000-1000-8000-00805F9B34FB";
+static NSString * const bgmRecordAccessControlPointCharacteristicUUIDString = @"00002A52-0000-1000-8000-00805F9B34FB";
+
+static NSString * const bpmServiceUUIDString = @"00001810-0000-1000-8000-00805F9B34FB";
+static NSString * const bpmBloodPressureMeasurementCharacteristicUUIDString = @"00002A35-0000-1000-8000-00805F9B34FB";
+static NSString * const bpmIntermediateCuffPressureCharacteristicUUIDString = @"00002A36-0000-1000-8000-00805F9B34FB";
+
+static NSString * const dfuServiceUUIDString = @"00001530-1212-EFDE-1523-785FEABCD123";
+static NSString * const dfuControlPointCharacteristicUUIDString = @"00001531-1212-EFDE-1523-785FEABCD123";
+static NSString * const dfuPacketCharacteristicUUIDString = @"00001532-1212-EFDE-1523-785FEABCD123";
+
 static NSString * const rscServiceUUIDString = @"00001814-0000-1000-8000-00805F9B34FB";
 static NSString * const rscMeasurementCharacteristicUUIDString = @"00002A53-0000-1000-8000-00805F9B34FB";
 
@@ -20,6 +32,10 @@ static NSString * const hrsSensorLocationCharacteristicUUIDString = @"00002A38-0
 
 static NSString * const htsServiceUUIDString = @"00001809-0000-1000-8000-00805F9B34FB";
 static NSString * const htsMeasurementCharacteristicUUIDString = @"00002A1C-0000-1000-8000-00805F9B34FB";
+
+static NSString * const proximityImmediateAlertServiceUUIDString = @"00001802-0000-1000-8000-00805F9B34FB";
+static NSString * const proximityLinkLossServiceUUIDString = @"00001803-0000-1000-8000-00805F9B34FB";
+static NSString * const proximityAlertLevelCharacteristicUUIDString = @"00002A06-0000-1000-8000-00805F9B34FB";
 
 static NSString * const batteryServiceUUIDString = @"0000180F-0000-1000-8000-00805F9B34FB";
 static NSString * const batteryLevelCharacteristicUUIDString = @"00002A19-0000-1000-8000-00805F9B34FB";
