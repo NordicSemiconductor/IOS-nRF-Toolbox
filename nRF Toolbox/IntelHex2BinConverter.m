8755f679d9f349f0806d26a0169b644005aa68b4
alno <alno@94a72e49-5737-764a-8bf3-0d5ceb61e552>
2014-07-02 11:13:06 +0000

    IntelHex2Bin converter fixed: - skipping jumps to non-following address, skipping addresses below 0x1000.

--- nRF Toolbox/IntelHex2BinConverter.m
+++ nRF Toolbox/IntelHex2BinConverter.m
@@ -28,6 +28,14 @@
     return (first << 4) | second;
 }
 
++(const UInt16)readAddress:(const Byte*)pointer
+{
+    Byte msb = [IntelHex2BinConverter readByte:pointer];
+    Byte lsb = [IntelHex2BinConverter readByte:pointer + 2];
+    
+    return (msb << 8) | lsb;
+}
+
 +(NSUInteger)calculateBinLength:(NSData*)hex
 {
     if (hex == nil || hex.length == 0)
@@ -38,6 +46,7 @@
     NSUInteger binLength = 0;
     const NSUInteger hexLength = hex.length;
     const Byte* pointer = (const Byte*)hex.bytes;
+    UInt16 lastULBA = 0;
     
     do
     {
@@ -50,13 +59,27 @@
         }
         
         const UInt8 reclen = [IntelHex2BinConverter readByte:pointer]; pointer += 2;
-        pointer += 4;   // Skip the offset
+        const UInt16 offset = [IntelHex2BinConverter readAddress:pointer]; pointer += 4;
         const UInt8 rectype = [IntelHex2BinConverter readByte:pointer]; pointer += 2;
         
-        // If record type is Data Record (rectype = 0), add it's length
-        if (rectype == 0)
-        {
-            binLength += reclen;
+        switch (rectype) {
+            case 0x04: {
+                // Only consistent hex files are supported. If there is a jump (0x04) to non-following address skip the rest of the file
+                const UInt16 newULBA = [IntelHex2BinConverter readAddress:pointer];
+                if (binLength > 0 && newULBA != lastULBA + 1)
+                    return binLength;
+                lastULBA = newULBA;
+                break;
+            }
+            case 0x02:
+                // Should here be the same as for 0x04?
+                break;
+            case 0x00:
+                // If record type is Data Record (rectype = 0), add it's length (only it the address is >= 0x1000, MBR is skipped)
+                if ((lastULBA << 16) + offset >= 0x1000)
+                    binLength += reclen;
+            default:
+                break;
         }
         
         pointer += (reclen << 1);  // Skip the data when calculating length
@@ -74,6 +97,8 @@
     const NSUInteger binLength = [IntelHex2BinConverter calculateBinLength:hex];
     const NSUInteger hexLength = hex.length;
     const Byte* pointer = (const Byte*)hex.bytes;
+    NSUInteger bytesCopied = 0;
+    UInt16 lastULBA = 0;
     
     Byte* bytes = malloc(sizeof(Byte) * binLength);
     Byte* output = bytes;
@@ -90,20 +115,35 @@
         }
         
         const UInt8 reclen = [IntelHex2BinConverter readByte:pointer]; pointer += 2;
-        pointer += 4;   // Skip the offset
+        const UInt16 offset = [IntelHex2BinConverter readAddress:pointer]; pointer += 4;
         const UInt8 rectype = [IntelHex2BinConverter readByte:pointer]; pointer += 2;
         
-        // If record type is Data Record (rectype = 0), copy data to output buffer
-        if (rectype == 0)
-        {
-            for (int i = 0; i < reclen; i++)
-            {
-                *output++ = [IntelHex2BinConverter readByte:pointer]; pointer += 2;
+        switch (rectype) {
+            case 0x04: {
+                // Only consistent hex files are supported. If there is a jump (0x04) to non-following address skip the rest of the file
+                const UInt16 newULBA = [IntelHex2BinConverter readAddress:pointer]; pointer += 4;
+                if (bytesCopied > 0 && newULBA != lastULBA + 1)
+                    return [NSData dataWithBytesNoCopy:bytes length:bytesCopied];
+                lastULBA = newULBA;
+                break;
             }
-        }
-        else
-        {
-            pointer += (reclen << 1);  // Skip the data when calculating length
+            case 0x00:
+                // If record type is Data Record (rectype = 0), copy data to output buffer
+                // Skip data below 0x1000 address (MBR)
+                if ((lastULBA << 16) + offset >= 0x1000)
+                {
+                    for (int i = 0; i < reclen; i++)
+                    {
+                        *output++ = [IntelHex2BinConverter readByte:pointer]; pointer += 2;
+                        bytesCopied++;
+                    }
+                }
+                break;
+            case 0x02:
+                // Should here be the same as for 0x04?
+            default:
+                pointer += (reclen << 1);  // Skip the data when calculating length
+                break;
         }
         
         pointer += 2;   // Skip the checksum
@@ -112,7 +152,7 @@
         if (*pointer == '\n') pointer++;
     } while (pointer != hex.bytes + hexLength);
     
-    return [NSData dataWithBytesNoCopy:bytes length:binLength];
+    return [NSData dataWithBytesNoCopy:bytes length:bytesCopied];
 }
 
 @end
