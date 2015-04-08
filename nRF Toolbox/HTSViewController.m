fd0a71e5d232141f36f2296fc46d0d05cb5bff35
Soomro, Kamran <Kamran.Soomro@nordicsemi.no>
2015-04-08 15:37:35 +0200

    Added License text

--- nRF Toolbox/HTSViewController.m
+++ nRF Toolbox/HTSViewController.m
@@ -1,10 +1,24 @@
-//
-//  HTSViewController.m
-//  nRF Toolbox
-//
-//  Created by Aleksander Nowakowski on 09/01/14.
-//  Copyright (c) 2014 Nordic Semiconductor. All rights reserved.
-//
+/*
+ * Copyright (c) 2015, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #import "HTSViewController.h"
 #import "ScannerViewController.h"
@@ -92,14 +106,14 @@
 
 -(void)appDidEnterBackground:(NSNotification *)_notification
 {
-    //    UILocalNotification *notification = [[UILocalNotification alloc]init];
-    //    notification.alertAction = @"Show";
-    //    notification.alertBody = @"You are still connected to Running Speed and Cadence sensor. It will collect data also in background.";
-    //    notification.hasAction = NO;
-    //    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:1];
-    //    notification.timeZone = [NSTimeZone  defaultTimeZone];
-    //    notification.soundName = UILocalNotificationDefaultSoundName;
-    //    [[UIApplication sharedApplication] setScheduledLocalNotifications:[NSArray arrayWithObject:notification]];
+    UILocalNotification *notification = [[UILocalNotification alloc]init];
+    notification.alertAction = @"Show";
+    notification.alertBody = @"You are still connected to Health Thermometer sensor. It will collect data also in background.";
+    notification.hasAction = NO;
+    notification.fireDate = [NSDate dateWithTimeIntervalSinceNow:1];
+    notification.timeZone = [NSTimeZone  defaultTimeZone];
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    [[UIApplication sharedApplication] setScheduledLocalNotifications:[NSArray arrayWithObject:notification]];
 }
 
 -(void)appDidBecomeActiveBackground:(NSNotification *)_notification
@@ -216,8 +230,8 @@
         [connectButton setTitle:@"DISCONNECT" forState:UIControlStateNormal];
     });
     
-    //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
-    //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActiveBackground:) name:UIApplicationDidBecomeActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidEnterBackground:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActiveBackground:) name:UIApplicationDidBecomeActiveNotification object:nil];
     
     // Peripheral has connected. Discover required services
     connectedPeripheral = peripheral;
@@ -245,8 +259,8 @@
         connectedPeripheral = nil;
         
         [self clearUI];
-        //[[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
-        //[[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationDidEnterBackgroundNotification object:nil];
     });
 }
 
