9ac80f9295addbd73bc060d58236ad2f617de363
Mostafa Berg <mostafa.berg@nordicsemi.no>
2016-05-02 15:57:14 +0200

    +Created Continueous glucose feature data class to encapsulate all the characteristics' information

---
+++ nRF Toolbox/BGM/Model/ContinuousGlucoseFeatureData.h
@@ -0,0 +1,13 @@
+//
+//  ContinuousGlucoseFeatureData.h
+//  nRF Toolbox
+//
+//  Created by Mostafa Berg on 02/05/16.
+//  Copyright © 2016 Nordic Semiconductor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContinuousGlucoseFeatureData : NSObject
+
+@end
---
+++ nRF Toolbox/BGM/Model/ContinuousGlucoseFeatureData.m
@@ -0,0 +1,13 @@
+//
+//  ContinuousGlucoseFeatureData.m
+//  nRF Toolbox
+//
+//  Created by Mostafa Berg on 02/05/16.
+//  Copyright © 2016 Nordic Semiconductor. All rights reserved.
+//
+
+#import "ContinuousGlucoseFeatureData.h"
+
+@implementation ContinuousGlucoseFeatureData
+
+@end
