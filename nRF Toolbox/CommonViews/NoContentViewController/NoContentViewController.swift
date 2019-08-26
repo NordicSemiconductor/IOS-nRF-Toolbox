6d97c616f3d08bae7ea17ce79bf91857f2f5c384
Nick Kibysh <nick.kibysh@nordicsemi.no>
2019-08-26 16:35:53 +0200

    removed unnecessary files renamed NoContentView -> InfoActionView

--- nRF Toolbox/CommonViews/NoContentViewController/NoContentViewController.swift
+++ nRF Toolbox/CommonViews/NoContentViewController/NoContentViewController.swift
@@ -15,7 +15,7 @@ class NoContentViewController: UIViewController {
         
         let image = UIImage(named: "Logo_Vertical_Transparent_White")?.withRenderingMode(.alwaysTemplate)
         
-        let messageView = NoContentView.instanceWithParams(image: image)
+        let messageView = InfoActionView.instanceWithParams(image: image)
         self.view = messageView
     }
     
--- nRF Toolbox/Utilities/Extensions/UIView+Ext.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UIView+Ext.swift
-//  nRF Toolbox
-//
-//  Created by Nick Kibysh on 26/08/2019.
-//  Copyright © 2019 Nordic Semiconductor. All rights reserved.
-//
-
-import Foundation
