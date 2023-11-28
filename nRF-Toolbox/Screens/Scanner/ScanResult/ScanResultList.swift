35b35e604d7c511c7617f0f125ccf7e38d71e2b8
Nick Kibysh <nick.kibysh@nordicsemi.no>
2023-11-28 16:00:50 +0100

    updated scanner selection

--- nRF-Toolbox/Screens/Scanner/ScanResult/ScanResultList.swift
+++ nRF-Toolbox/Screens/Scanner/ScanResult/ScanResultList.swift
@@ -10,46 +10,74 @@ import SwiftUI
 import iOS_Bluetooth_Numbers_Database
 
 fileprivate typealias Env = PeripheralScannerScreen.PeripheralScannerViewModel.Environment
+fileprivate typealias ScanResult = PeripheralScannerScreen.PeripheralScannerViewModel.ScanResult
 
 struct ScanResultList: View {
+    
     @EnvironmentObject private var environment: Env
     @Environment(\.dismiss) var dismiss
+    @State private var selectedDevice: ScanResult.ID?
+    
+    @ViewBuilder
+    private func deviceLabel(device: ScanResult) -> some View {
+        HStack {
+            ScanResultItem(name: device.name, rssi: device.rssi, services: device.services)
+            Spacer()
+            if environment.connectingDevice == device {
+                ProgressView()
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func deviceView(device: ScanResult) -> some View {
+        if #available(macOS 14.0, *) {
+            deviceLabel(device: device)
+        } else {
+            Button {
+                Task {
+                    await environment.connect(device)
+                    dismiss()
+                }
+            } label: {
+                deviceLabel(device: device)
+            }
+            .buttonStyle(PlainButtonStyle())
+#if os(macOS)
+            Divider()
+#endif
+        }
+    }
+    
+    @ViewBuilder
+    private var devicesSection: some View {
+        Section {
+            ForEach(environment.devices) { device in
+                deviceView(device: device)
+            }
+        } footer: {
+            Text("Select the device to establish connection")
+        }
+    }
     
     var body: some View {
-        List {
-            Section {
-                ForEach(environment.devices) { device in
-                    VStack {
-                        Button {
-                            Task {
-                                await environment.connect(device)
-                                dismiss()
-                            }
-                        } label: {
-                            HStack {
-                                ScanResultItem(name: device.name, rssi: device.rssi, services: device.services)
-                                Spacer()
-                                if environment.connectingDevice == device {
-                                    ProgressView()
-                                } else {
-                                    Button {
-                                        // TODO: Open info screen
-                                    } label: {
-                                        Image(systemName: "info.circle")
-                                    }
-                                }
-                            }
-                        }
-                        #if os(macOS)
-                        Divider()
-                        #endif
-                    }
-                }
-                .buttonStyle(PlainButtonStyle())
-            } footer: {
-                Text("Select the device to establish connection or press 􀅴 to open detailed information")
+        if #available(macOS 14.0, iOS 17, *) {
+            List(selection: $selectedDevice) {
+                devicesSection
             }
-
+            .onChange(of: selectedDevice) { oldValue, newValue in
+                guard let deviceId = newValue else { return }
+                guard let device = environment.devices.first(where: { $0.id == deviceId }) else { return }
+                
+                Task {
+                    await environment.connect(device)
+                    dismiss()
+                }
+            }
+        } else {
+            List {
+                devicesSection
+            }
         }
     }
 }
