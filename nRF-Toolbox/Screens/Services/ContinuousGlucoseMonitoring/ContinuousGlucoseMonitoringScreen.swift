7b20f32c7802a4875d165166f7d7213c102fd998
Nick Kibysh <nick.kibysh@nordicsemi.no>
2024-08-06 20:20:42 +0200

    feat(cgms): Created template files

---
+++ nRF-Toolbox/Screens/Services/ContinuousGlucoseMonitoring/ContinuousGlucoseMonitoringScreen.swift
@@ -0,0 +1,19 @@
+//
+//  ContinuousGlucoseMonitoringScreen.swift
+//  nRF-Toolbox
+//
+//  Created by Nick Kibysh on 06/08/2024.
+//  Copyright © 2024 Nordic Semiconductor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContinuousGlucoseMonitoringScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContinuousGlucoseMonitoringScreen()
+}
---
+++ nRF-Toolbox/Screens/Services/ContinuousGlucoseMonitoring/ContinuousGlucoseMonitoringViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ContinuousGlucoseMonitoringViewModel.swift
+//  nRF-Toolbox
+//
+//  Created by Nick Kibysh on 06/08/2024.
+//  Copyright © 2024 Nordic Semiconductor. All rights reserved.
+//
+
+import Foundation
