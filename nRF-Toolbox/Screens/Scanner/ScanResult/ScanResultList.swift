0df77177f8cdc8844314ac1d52c56222363db658
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-11-21 14:13:24 +0000

    Minor ScanResultList changes
    Just moving things around. That's it.

--- nRF-Toolbox/Screens/Scanner/ScanResult/ScanResultList.swift
+++ nRF-Toolbox/Screens/Scanner/ScanResult/ScanResultList.swift
@@ -12,53 +12,15 @@ import iOS_Bluetooth_Numbers_Database
 fileprivate typealias Env = PeripheralScannerScreen.PeripheralScannerViewModel.Environment
 fileprivate typealias ScanResult = PeripheralScannerScreen.PeripheralScannerViewModel.ScanResult
 
+// MARK: - ScanResultList
+
 struct ScanResultList: View {
     
     @EnvironmentObject private var environment: Env
     @Environment(\.dismiss) var dismiss
     @State private var selectedDevice: ScanResult.ID?
     
-    @ViewBuilder
-    private func deviceLabel(device: ScanResult) -> some View {
-        HStack {
-            ScanResultItem(name: device.name, rssi: device.rssi, services: device.services)
-            Spacer()
-            if environment.connectingDevice == device {
-                ProgressView()
-            }
-        }
-    }
-    
-    @ViewBuilder
-    private func deviceView(device: ScanResult) -> some View {
-        if #available(macOS 14.0, *) {
-            deviceLabel(device: device)
-        } else {
-            Button {
-                Task {
-                    await environment.connect(device)
-                    dismiss()
-                }
-            } label: {
-                deviceLabel(device: device)
-            }
-            .buttonStyle(PlainButtonStyle())
-#if os(macOS)
-            Divider()
-#endif
-        }
-    }
-    
-    @ViewBuilder
-    private var devicesSection: some View {
-        Section {
-            ForEach(environment.devices) { device in
-                deviceView(device: device)
-            }
-        } footer: {
-            Text("Select the device to establish connection")
-        }
-    }
+    // MARK: view
     
     var body: some View {
         if #available(macOS 14.0, iOS 17, *) {
@@ -80,13 +42,52 @@ struct ScanResultList: View {
             }
         }
     }
-}
-
-#Preview {
-    ScanResultList()
-        .environmentObject(Env(devices: [
-            PeripheralScannerScreen.PeripheralScannerViewModel.ScanResult(name: "Device", rssi: -59, id: UUID(), services: []),
-            PeripheralScannerScreen.PeripheralScannerViewModel.ScanResult(name: "Device", rssi: -69, id: UUID(), services: []),
-            PeripheralScannerScreen.PeripheralScannerViewModel.ScanResult(name: "Device", rssi: -79, id: UUID(), services: []),
-        ]))
+    
+    // MARK: deviceLabel
+    
+    @ViewBuilder
+    private func deviceLabel(device: ScanResult) -> some View {
+        HStack {
+            ScanResultItem(name: device.name, rssi: device.rssi, services: device.services)
+            Spacer()
+            if environment.connectingDevice == device {
+                ProgressView()
+            }
+        }
+    }
+    
+    // MARK: deviceView
+    
+    @ViewBuilder
+    private func deviceView(device: ScanResult) -> some View {
+        if #available(macOS 14.0, *) {
+            deviceLabel(device: device)
+        } else {
+            Button {
+                Task {
+                    await environment.connect(device)
+                    dismiss()
+                }
+            } label: {
+                deviceLabel(device: device)
+            }
+            .buttonStyle(PlainButtonStyle())
+#if os(macOS)
+            Divider()
+#endif
+        }
+    }
+    
+    // MARK: devicesSection
+    
+    @ViewBuilder
+    private var devicesSection: some View {
+        Section {
+            ForEach(environment.devices) { device in
+                deviceView(device: device)
+            }
+        } footer: {
+            Text("Select the device to establish connection")
+        }
+    }
 }
