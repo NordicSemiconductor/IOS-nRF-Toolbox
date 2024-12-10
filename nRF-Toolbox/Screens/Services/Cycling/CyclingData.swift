e0987783b28df38cbdbdc273ded3867d4a88cc4b
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2024-12-10 12:17:04 +0000

    Refactor: CyclingCharacteristic is now CyclingData
    Now we can try to... parse data?

---
+++ nRF-Toolbox/Screens/Services/Cycling/CyclingData.swift
@@ -0,0 +1,117 @@
+//
+//  CyclingData.swift
+//  nRF-Toolbox
+//
+//  Created by Dinesh Harjani on 10/12/24.
+//  Copyright © 2024 Nordic Semiconductor. All rights reserved.
+//
+
+import Foundation
+
+private extension Flag {
+    static let wheelData: Flag = 0x01
+    static let crankData: Flag = 0x02
+}
+
+// MARK: - CyclingData
+
+struct CyclingData {
+    
+    typealias WheelRevolutionAndTime = (revolution: Int, time: Double)
+    
+    let wheelRevolutionsAndTime: WheelRevolutionAndTime?
+    let crankRevolutionsAndTime: WheelRevolutionAndTime?
+    
+    static let zero = CyclingData(wheelRevolutionsAndTime: (0, 0.0), crankRevolutionsAndTime: (0, 0.0))
+    
+    // MARK: init
+    
+    init(wheelRevolutionsAndTime: (Int, Double)?, crankRevolutionsAndTime: (Int, Double)?) {
+        self.wheelRevolutionsAndTime = wheelRevolutionsAndTime
+        self.crankRevolutionsAndTime = crankRevolutionsAndTime
+    }
+    
+    init(_ data: Data) throws {
+        let flags: UInt8 = try data.read()
+        
+        wheelRevolutionsAndTime = try Flag.isAvailable(bits: flags, flag: .wheelData) ? {
+                (
+                    Int(try data.read(fromOffset: 1) as UInt32),
+                    Double(try data.read(fromOffset: 5) as UInt16)
+                )
+            }() : nil
+        
+        let crankOffset: (Int, Int) = Flag.isAvailable(bits: flags, flag: .wheelData) ? (7, 9) : (1, 3)
+        
+        crankRevolutionsAndTime = try Flag.isAvailable(bits: flags, flag: .crankData) ? {
+                (
+                    Int(try data.read(fromOffset: crankOffset.0) as UInt16),
+                    Double(try data.read(fromOffset: crankOffset.1) as UInt16)
+                )
+            }() : nil
+    }
+    
+    func travelDistance(with wheelCircumference: Double) -> Measurement<UnitLength>? {
+        wheelRevolutionsAndTime.map { Measurement<UnitLength>(value: Double($0.revolution) * wheelCircumference, unit: .meters) }
+    }
+    
+    func distance(_ oldCharacteristic: CyclingData, wheelCircumference: Double) -> Measurement<UnitLength>? {
+        wheelRevolutionDiff(oldCharacteristic)
+            .flatMap { Measurement<UnitLength>(value: Double($0) * wheelCircumference, unit: .meters) }
+    }
+    
+    func gearRatio(_ oldCharacteristic: CyclingData) -> Double? {
+        guard let wheelRevolutionDiff = wheelRevolutionDiff(oldCharacteristic), let crankRevolutionDiff = crankRevolutionDiff(oldCharacteristic), crankRevolutionDiff != 0 else {
+            return nil
+        }
+        return Double(wheelRevolutionDiff) / Double(crankRevolutionDiff)
+    }
+    
+    func speed(_ oldCharacteristic: CyclingData, wheelCircumference: Double) -> Measurement<UnitSpeed>? {
+        guard let wheelRevolutionDiff = wheelRevolutionDiff(oldCharacteristic), let wheelEventTime = wheelRevolutionsAndTime?.time, let oldWheelEventTime = oldCharacteristic.wheelRevolutionsAndTime?.time else {
+            return nil
+        }
+        
+        var wheelEventTimeDiff = wheelEventTime - oldWheelEventTime
+        guard wheelEventTimeDiff > 0 else {
+            return nil
+        }
+        
+        wheelEventTimeDiff /= 1024
+        let speed = (Double(wheelRevolutionDiff) * wheelCircumference) / wheelEventTimeDiff
+        return Measurement<UnitSpeed>(value: speed, unit: .milesPerHour)
+    }
+    
+    func cadence(_ oldCharacteristic: CyclingData) -> Int? {
+        guard let crankRevolutionDiff = crankRevolutionDiff(oldCharacteristic), let crankEventTimeDiff = crankEventTimeDiff(oldCharacteristic), crankEventTimeDiff > 0 else {
+            return nil
+        }
+        
+        return Int(Double(crankRevolutionDiff) / crankEventTimeDiff * 60.0)
+    }
+    
+    // MARK: - Private
+    
+    private func wheelRevolutionDiff(_ oldCharacteristic: CyclingData) -> Int? {
+        guard let oldWheelRevolution = oldCharacteristic.wheelRevolutionsAndTime?.revolution, let wheelRevolution = wheelRevolutionsAndTime?.revolution else {
+            return nil
+        }
+        guard oldWheelRevolution != 0 else { return 0 }
+        return wheelRevolution - oldWheelRevolution
+    }
+    
+    private func crankRevolutionDiff(_ old: CyclingData) -> Int? {
+        guard let crankRevolution = crankRevolutionsAndTime?.revolution, let oldCrankRevolution = old.crankRevolutionsAndTime?.revolution else {
+            return nil
+        }
+        
+        return crankRevolution - oldCrankRevolution
+    }
+    
+    private func crankEventTimeDiff(_ old: CyclingData) -> Double? {
+        guard let crankEventTime = crankRevolutionsAndTime?.time, let oldCrankEventTime = old.crankRevolutionsAndTime?.time else {
+            return nil
+        }
+        return (crankEventTime - oldCrankEventTime) / 1024.0
+    }
+}
--- nRF-Toolbox/Screens/Services/Cycling/CyclingServiceViewModel.swift
+++ nRF-Toolbox/Screens/Services/Cycling/CyclingServiceViewModel.swift
@@ -21,6 +21,8 @@ final class CyclingServiceViewModel: ObservableObject {
     
     // MARK: Properties
     
+    @Published private(set) var data: CyclingData?
+    
     private let service: CBService
     private let peripheral: Peripheral
     private var cscMeasurement: CBCharacteristic!
@@ -80,7 +82,8 @@ final class CyclingServiceViewModel: ObservableObject {
         
         log.debug("Detected \(cyclingFeatures)")
         peripheral.listenValues(for: cscMeasurement)
-            .compactMap { try? CyclingCharacteristic($0) }
+            .compactMap { try? CyclingData($0) }
+            .receive(on: DispatchQueue.main)
             .sink { [unowned self] completion in
                 switch completion {
                 case .finished:
@@ -90,10 +93,12 @@ final class CyclingServiceViewModel: ObservableObject {
                 }
             } receiveValue: { [unowned self] update in
                 self.log.debug("\(update)")
+                self.data = update
             }
             .store(in: &cancellables)
         
         // Enable Notifications
+        log.debug("Enabling Cycling Speed & Cadence Notifications...")
         _ = try await peripheral.setNotifyValue(true, for: cscMeasurement).firstValue
     }
 }
@@ -122,109 +127,3 @@ extension CyclingServiceViewModel: SupportedServiceViewModel {
         cancellables.removeAll()
     }
 }
-
-private extension Flag {
-    static let wheelData: Flag = 0x01
-    static let crankData: Flag = 0x02
-}
-
-// MARK: - CyclingCharacteristic
-
-struct CyclingCharacteristic {
-    
-    typealias WheelRevolutionAndTime = (revolution: Int, time: Double)
-    
-    let wheelRevolutionsAndTime: WheelRevolutionAndTime?
-    let crankRevolutionsAndTime: WheelRevolutionAndTime?
-    
-    static let zero = CyclingCharacteristic(wheelRevolutionsAndTime: (0, 0.0), crankRevolutionsAndTime: (0, 0.0))
-    
-    init(wheelRevolutionsAndTime: (Int, Double)?, crankRevolutionsAndTime: (Int, Double)?) {
-        self.wheelRevolutionsAndTime = wheelRevolutionsAndTime
-        self.crankRevolutionsAndTime = crankRevolutionsAndTime
-    }
-    
-    init(_ data: Data) throws {
-        let flags: UInt8 = try data.read()
-        
-        wheelRevolutionsAndTime = try Flag.isAvailable(bits: flags, flag: .wheelData) ? {
-                (
-                    Int(try data.read(fromOffset: 1) as UInt32),
-                    Double(try data.read(fromOffset: 5) as UInt16)
-                )
-            }() : nil
-        
-        let crankOffset: (Int, Int) = Flag.isAvailable(bits: flags, flag: .wheelData) ? (7, 9) : (1, 3)
-        
-        crankRevolutionsAndTime = try Flag.isAvailable(bits: flags, flag: .crankData) ? {
-                (
-                    Int(try data.read(fromOffset: crankOffset.0) as UInt16),
-                    Double(try data.read(fromOffset: crankOffset.1) as UInt16)
-                )
-            }() : nil
-    }
-    
-    func travelDistance(with wheelCircumference: Double) -> Measurement<UnitLength>? {
-        wheelRevolutionsAndTime.map { Measurement<UnitLength>(value: Double($0.revolution) * wheelCircumference, unit: .meters) }
-    }
-    
-    func distance(_ oldCharacteristic: CyclingCharacteristic, wheelCircumference: Double) -> Measurement<UnitLength>? {
-        wheelRevolutionDiff(oldCharacteristic)
-            .flatMap { Measurement<UnitLength>(value: Double($0) * wheelCircumference, unit: .meters) }
-    }
-    
-    func gearRatio(_ oldCharacteristic: CyclingCharacteristic) -> Double? {
-        guard let wheelRevolutionDiff = wheelRevolutionDiff(oldCharacteristic), let crankRevolutionDiff = crankRevolutionDiff(oldCharacteristic), crankRevolutionDiff != 0 else {
-            return nil
-        }
-        return Double(wheelRevolutionDiff) / Double(crankRevolutionDiff)
-    }
-    
-    func speed(_ oldCharacteristic: CyclingCharacteristic, wheelCircumference: Double) -> Measurement<UnitSpeed>? {
-        guard let wheelRevolutionDiff = wheelRevolutionDiff(oldCharacteristic), let wheelEventTime = wheelRevolutionsAndTime?.time, let oldWheelEventTime = oldCharacteristic.wheelRevolutionsAndTime?.time else {
-            return nil
-        }
-        
-        var wheelEventTimeDiff = wheelEventTime - oldWheelEventTime
-        guard wheelEventTimeDiff > 0 else {
-            return nil
-        }
-        
-        wheelEventTimeDiff /= 1024
-        let speed = (Double(wheelRevolutionDiff) * wheelCircumference) / wheelEventTimeDiff
-        return Measurement<UnitSpeed>(value: speed, unit: .milesPerHour)
-    }
-    
-    func cadence(_ oldCharacteristic: CyclingCharacteristic) -> Int? {
-        guard let crankRevolutionDiff = crankRevolutionDiff(oldCharacteristic), let crankEventTimeDiff = crankEventTimeDiff(oldCharacteristic), crankEventTimeDiff > 0 else {
-            return nil
-        }
-        
-        return Int(Double(crankRevolutionDiff) / crankEventTimeDiff * 60.0)
-    }
-    
-    private func wheelRevolutionDiff(_ oldCharacteristic: CyclingCharacteristic) -> Int? {
-        guard let oldWheelRevolution = oldCharacteristic.wheelRevolutionsAndTime?.revolution, let wheelRevolution = wheelRevolutionsAndTime?.revolution else {
-            return nil
-        }
-        guard oldWheelRevolution != 0 else { return 0 }
-        return wheelRevolution - oldWheelRevolution
-    }
-    
-    private func crankRevolutionDiff(_ old: CyclingCharacteristic) -> Int? {
-        guard let crankRevolution = crankRevolutionsAndTime?.revolution, let oldCrankRevolution = old.crankRevolutionsAndTime?.revolution else {
-            return nil
-        }
-        
-        return crankRevolution - oldCrankRevolution
-    }
-    
-    private func crankEventTimeDiff(_ old: CyclingCharacteristic) -> Double? {
-        guard let crankEventTime = crankRevolutionsAndTime?.time, let oldCrankEventTime = old.crankRevolutionsAndTime?.time else {
-            return nil
-        }
-        return (crankEventTime - oldCrankEventTime) / 1024.0
-    }
-}
-
-
