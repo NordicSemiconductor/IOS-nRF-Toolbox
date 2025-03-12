8182d564813b9a9a0d1b2f4503a22af80e0ccb08
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-03-12 14:31:14 +0000

    AttributeList is now closer to what we intended

--- nRF-Toolbox/Screens/DeviceDetails/AttributeTable/AttributeList/AttributeList.swift
+++ nRF-Toolbox/Screens/DeviceDetails/AttributeTable/AttributeList/AttributeList.swift
@@ -34,9 +34,29 @@ struct AttributeList: View {
             List {
                 ForEach(attributeTable.services) { service in
                     Section("") {
-                        AttributeItemView(attribute: service)
-                        
-                        // TODO: Service Attributes
+                        if service.characteristics.hasItems {
+                            DisclosureGroup {
+                                ForEach(service.characteristics) { characteristic in
+                                    if characteristic.descriptors.hasItems {
+                                        DisclosureGroup {
+                                            ForEach(characteristic.descriptors) { descriptor in
+                                                AttributeItemView(attribute: descriptor)
+                                            }
+                                        } label: {
+                                            AttributeItemView(attribute: characteristic)
+                                        }
+                                        .accentColor(.nordicMiddleGrey)
+                                    } else {
+                                        AttributeItemView(attribute: characteristic)
+                                    }
+                                }
+                            } label: {
+                                AttributeItemView(attribute: service)
+                            }
+                            .accentColor(.nordicMiddleGrey)
+                        } else {
+                            AttributeItemView(attribute: service)
+                        }
                     }
                 }
             }
