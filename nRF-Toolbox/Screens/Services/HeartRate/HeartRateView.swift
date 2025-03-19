4ec6168c6b9c6723814673491a2cdd0fbf56cb8f
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-03-19 14:03:14 +0000

    Unified "HeartRateView" and "HeartRateChart" files
    No need for two files, I think. I might come back on that. Regardless, we did also fix something, which was the force unwrap on the last Heart Rate value, which I find it easy to imagine that it could cause problems.

--- nRF-Toolbox/Screens/Services/HeartRate/HeartRateView.swift
+++ nRF-Toolbox/Screens/Services/HeartRate/HeartRateView.swift
@@ -2,11 +2,50 @@
 //  HeartRateView.swift
 //  nRF-Toolbox
 //
+//  Created by Nick Kibysh on 25/10/2023.
 //  Created by Dinesh Harjani on 5/12/24.
 //  Copyright © 2024 Nordic Semiconductor. All rights reserved.
 //
 
 import SwiftUI
+import Charts
+
+// MARK: - HeartRateChart
+
+struct HeartRateChart: View {
+    
+    // MARK: Environment
+    
+    @EnvironmentObject private var viewModel: DeviceScreen.HeartRateViewModel
+    
+    // MARK: view
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Chart {
+                ForEach(viewModel.data, id: \.date) {
+                    LineMark(
+                        x: .value("Date", $0.date),
+                        y: .value("Heart Rate", $0.heartRate)
+                    )
+                    .foregroundStyle(.red)
+                }
+                .interpolationMethod(.catmullRom)
+            }
+            .chartXAxis(.hidden)
+            .chartYScale(domain: [viewModel.lowest, viewModel.highest],
+                         range: .plotDimension(padding: 8))
+            .chartScrollableAxes(.horizontal)
+            .chartXVisibleDomain(length: viewModel.visibleDomain)
+            .chartScrollPosition(x: $viewModel.scrolPosition)
+            
+            if let heartRate = viewModel.data.last?.heartRate {
+                Label("\(heartRate) bpm", systemImage: "waveform.path.ecg")
+                    .font(.title2.bold())
+            }
+        }
+    }
+}
 
 // MARK: - HeartRateView
 
