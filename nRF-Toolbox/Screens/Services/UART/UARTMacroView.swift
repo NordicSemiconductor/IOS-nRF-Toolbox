d79a96606c05ea341e02b1f8814a76d3018157a2
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-05-13 02:12:36 +0100

    Attempt at Command Symbols
    Doesn't show up. We need a picker for the End of Line, and I think a Horizontal Scroll View of sorts to select the Symbol. That should work.

--- nRF-Toolbox/Screens/Services/UART/UARTMacroView.swift
+++ nRF-Toolbox/Screens/Services/UART/UARTMacroView.swift
@@ -12,6 +12,21 @@ import SwiftUI
 
 struct UARTMacroView: View {
     
+    // MARK: Constant
+    
+    // End of line: LF | CR | CR+LF
+    
+    private static let availableSymbols: [String] = [
+        // left arrow, up arrow, right arrow, down arrow, gear
+        "chevron.left", "chevron.up", "chevron.right", "chevron.down", "gear",
+        // rewind, play, pause, stop, fast forward
+        "backward.fill", "play.fill", "pause.fill", "stop.fill", "forward.fill",
+        // info.circle, 1, 2, 3, 4
+        "info.circle", "1.circle", "2.circle", "3.circle", "4.circle",
+        // 5, 6, 7, 8, 9
+        "5.circle", "6.circle", "7.circle", "8.circle", "9.circle"
+    ]
+    
     // MARK: Properties
     
     @State private var showEditCommandAlert: Bool = false
@@ -58,20 +73,39 @@ struct UARTMacroView: View {
                     showEditCommandAlert = false
                 }
             
-            Divider()
+//            Divider()
+//            
+//            Text("End-of-Line")
+//            
+//            Divider()
+//            
+//            Text("Associated Symbol")
             
-            Text("Associated Symbol")
-            
-            Divider()
-            
-            Button("Cancel", role: .cancel) {
-                showEditCommandAlert = false
+            Grid {
+                ForEach(0..<4) { row in
+                    GridRow {
+                        ForEach(0..<5) { col in
+                            Button(action: {
+                                //
+                            }, label: {
+                                Image(systemName: Self.availableSymbols[row * 3 + col])
+                                    .resizable()
+                                    .frame(size: CGSize(asSquare: 22.0))
+                            })
+                            .tint(Color.nordicBlue)
+                            .buttonStyle(.bordered)
+                        }
+                    }
+                }
             }
             
-            Button("Save") {
-                
-                showEditCommandAlert = false
-            }
+//            Button("Cancel", role: .cancel) {
+//                showEditCommandAlert = false
+//            }
+//            
+//            Button("Save") {
+//                showEditCommandAlert = false
+//            }
         }
     }
 }
