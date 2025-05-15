d2b8c24ab0fec82b3bfab12212696fd93a23fbed
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-05-15 10:05:11 +0100

    Initial Implementation of XML Import / Export
    Not tested. But it's a step. And also obviously not finished.

--- nRF-Toolbox/Screens/Services/UART/UARTMacro+XML.swift
+++ nRF-Toolbox/Screens/Services/UART/UARTMacro+XML.swift
@@ -9,62 +9,86 @@
 import SwiftUI
 import AEXML
 
+// MARK: - UARTMacro
+
 extension UARTMacro {
     
-//    var document: AEXMLDocument {
-//        let doc = AEXMLDocument()
-//        let root = AEXMLElement(name: "uart-configuration", attributes: [
-//            "name":name
-//        ])
-//        let commands = AEXMLElement(name: "commands", attributes: [
-//            "length":"9"
-//        ])
-//        commands.addChildren(self.commands.map { $0.xml })
-//        root.addChild(commands)
-//        doc.addChild(root)
-//        
-//        return doc
-//    }
-//    
-//    init(data: Data) throws {
-//        let document = try AEXMLDocument(xml: data)
-//        let children = document.children
-//        guard let rootNode = children.first(where: { (e) -> Bool in
-//            e.name == "uart-configuration"
-//        }) else {
-//            throw Error.wrongXMLFormat
-//        }
-//        
-//        name = rootNode.attributes["name"] ?? ""
-//        guard let commandsNode = rootNode.children.first(where: { $0.name == "commands" }) else {
-//            throw Error.wrongXMLFormat
-//        }
-//        
-//        var commands: [UARTCommandModel] = []
-//        for node in commandsNode.children {
-//            guard commands.count < 9 else {
-//                break
-//            }
-//            
-//            guard let text = node.value else {
-//                commands.append(EmptyModel())
-//                continue
-//            }
-//            
-//            let image = CommandImage(name: (node.attributes["icon"] ?? ""), modernIcon: node.attributes["system_icon"].map({ModernIcon(name: $0)}))
+    enum XMLError: Error {
+        case wrongXMLFormat
+    }
+    
+    // MARK: init (Import)
+    
+    init(data: Data) throws {
+        let document = try AEXMLDocument(xml: data)
+        let children = document.children
+        guard let rootNode = children.first(where: { (e) -> Bool in
+            e.name == "uart-configuration"
+        }) else {
+            throw XMLError.wrongXMLFormat
+        }
+        
+        name = rootNode.attributes["name"] ?? ""
+        guard let commandsNode = rootNode.children.first(where: { $0.name == "commands" }) else {
+            throw XMLError.wrongXMLFormat
+        }
+        
+        var commands: [UARTMacroCommand] = []
+        commands.reserveCapacity(UARTMacro.numberOfCommands)
+        for (i, node) in commandsNode.children.enumerated() where i < UARTMacro.numberOfCommands {
+            guard let text = node.value else {
+                commands.append(UARTMacroCommand(i))
+                continue
+            }
+            
+            // TODO: Pending.
+//            let image = CommandImage(name: (node.attributes["icon"] ?? ""), modernIcon: node.attributes["system_icon"].map({ ModernIcon(name: $0) }))
 //
 //            if let type = node.attributes["type"], type == "data" {
 //                commands.append(DataCommand(data: Data(text.hexa), image: image))
 //            } else {
 //                commands.append(TextCommand(text: text, image: image))
 //            }
-//
-//        }
-//        
-//        while commands.count < 9 {
-//            commands.append(EmptyModel())
-//        }
-//        
-//        self.commands = commands
-//    }
+        }
+        
+        for i in commands.count..<UARTMacro.numberOfCommands {
+            commands.append(UARTMacroCommand(i))
+        }
+        
+        self.commands = commands
+    }
+    
+    // MARK: xml (Export)
+    
+    var xml: AEXMLDocument {
+        let doc = AEXMLDocument()
+        let root = AEXMLElement(name: "uart-configuration", attributes: [
+            "name": name
+        ])
+        let commands = AEXMLElement(name: "commands", attributes: [
+            "length": "9"
+        ])
+        commands.addChildren(self.commands.map {
+            $0.xml
+        })
+        root.addChild(commands)
+        doc.addChild(root)
+        
+        return doc
+    }
+}
+
+// MARK: - UARTMacroCommand
+
+extension UARTMacroCommand {
+    
+    var xml: AEXMLElement {
+        AEXMLElement(name: "command", value: command, attributes: [
+            "icon": symbol,
+            "active": "true",
+            "eol": eol.description,
+            "type": "text",
+            "system_icon": ""
+        ])
+    }
 }
