903c28809f21b0d283abd55ceb34224c872442cd
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-07-21 13:04:33 +0100

    Refactor: RSCSPeripheralSpecDelegate is in its own file
    RSCS file is cluttered and I don't know where things are. We're trying to move things around so we can find what we need when we need it.

--- nRF-Toolbox/Screens/Services/Running/RSCS.swift
+++ nRF-Toolbox/Screens/Services/Running/RSCS.swift
@@ -18,7 +18,6 @@ typealias RSCFeature = RunningSpeedAndCadence.RSCFeature
 typealias SupportedSensorLocationsResponse = RunningSpeedAndCadence.SupportedSensorLocations
 
 extension CBMUUID {
-    
     static let rscMeasurement = CBMUUID(characteristic: .rscMeasurement)
     
     static let rscFeature = CBMUUID(characteristic: .rscFeature)
@@ -57,6 +56,7 @@ private extension CBMCharacteristicMock {
 }
 
 internal extension CBMServiceMock {
+    
     static let runningSpeedCadence = CBMServiceMock(
         type: .runningSpeedCadence,
         primary: true,
@@ -65,6 +65,7 @@ internal extension CBMServiceMock {
 }
 
 public extension RunningSpeedAndCadence {
+    
     enum ErrorCode: UInt8, LocalizedError {
         case procedureAlreadyInProgress = 0x80
         case descriptorImproperlyConfigured = 0x81
@@ -514,73 +515,7 @@ extension RunningSpeedAndCadence: RSCSDelegate {
     }
 }
 
-public class RSCSCBMPeripheralSpecDelegate: CBMPeripheralSpecDelegate {
-    
-    var enabledFeatures: RunningSpeedAndCadence.RSCFeature = .all
-    var sensorLocation: RunningSpeedAndCadence.SensorLocation = .inShoe
-    
-    var notifySCControlPoint: Bool = false
-    var measurementTimer: Timer?
-
-    weak var delegate: RSCSDelegate?
-    
-    enum MockError: Error {
-        case notifyIsNotSupported, readingIsNotSupported, writingIsNotSupported
-    }
-    
-    public func peripheral(_ peripheral: CBMPeripheralSpec,
-                    didReceiveReadRequestFor characteristic: CBMCharacteristicMock)
-    -> Result<Data, Error> {
-        if characteristic.uuid == CBMUUID.rscFeature {
-            return .success(Data([0xff, enabledFeatures.rawValue])) // Support all features
-        } else if characteristic.uuid == CBMUUID.sensorLocation {
-            return .success(Data([sensorLocation.rawValue]))
-        } else {
-            return .failure(MockError.readingIsNotSupported)
-        }
-    }
-    
-    public func peripheral(_ peripheral: CBMPeripheralSpec,
-                    didReceiveWriteRequestFor characteristic: CBMCharacteristicMock,
-                    data: Data) -> Result<Void, Error> {
-        if characteristic.uuid == CBMUUID.scControlPoint {
-            let opCode = RunningSpeedAndCadence.OpCode(rawValue: data.read(offset: 0))!
-            switch opCode {
-            case .setCumulativeValue:
-                let value: UInt32 = data.read(offset: 1)
-                delegate?.didReceiveSetCumulativeValue(value: value)
-            case .startSensorCalibration:
-                delegate?.didReceiveStartSensorCalibration()
-            case .updateSensorLocation:
-                let location = RunningSpeedAndCadence.SensorLocation(rawValue: data.read(offset: 1))!
-                delegate?.didReceiveUpdateSensorLocation(location)
-            case .requestSupportedSensorLocations:
-                delegate?.didReceiveRequestSupportedSensorLocations()
-            default:
-                return .failure(MockError.writingIsNotSupported)
-            }
-        } else {
-            return .failure(MockError.writingIsNotSupported)
-        }
-
-        return .success(())
-    }
-    
-    public func peripheral(_ peripheral: CBMPeripheralSpec, didReceiveSetNotifyRequest enabled: Bool, for characteristic: CBMCharacteristicMock) -> Result<Void, Error> {
-        switch characteristic.uuid {
-        case .rscMeasurement:
-            delegate?.measurementNotificationStatusChanged(enabled)
-        case .scControlPoint:
-            delegate?.controlPointNotificationStatusChanged(enabled)
-        default:
-            return .failure(MockError.notifyIsNotSupported)
-        }
-        
-        return .success(())
-    }
-}
-
-fileprivate extension Data {
+internal extension Data {
     
     func read<R: FixedWidthInteger>(offset: Int = 0) -> R {
         let length = MemoryLayout<R>.size
