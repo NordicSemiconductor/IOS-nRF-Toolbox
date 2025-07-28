1a97e8873fda5832ec55e15ac5033db653b987fa
Dinesh Harjani <dinesh.harjani@nordicsemi.no>
2025-07-28 12:36:50 +0100

    RunningServiceView(Screen) now shows Features
    This was missing, since Android shows them. I tried to look for similar things for the other Services, to try to copy the design, but I could not. We did fix the button for "Sensor Calibration", though.

--- nRF-Toolbox/Screens/Services/Running/RunningServiceScreen.swift
+++ nRF-Toolbox/Screens/Services/Running/RunningServiceScreen.swift
@@ -18,30 +18,34 @@ struct RunningServiceView: View {
     
     // MARK: Properties
     
+    private static let list = ListFormatter()
+    
     @State private var showSensorCalibration = false
     
     // MARK: view
     
     var body: some View {
-        Section {
-            RunningValuesGrid()
+        RunningValuesGrid()
+        
+        if let featureList = Self.list.string(from: environment.features.toArray()) {
+            Label(featureList, systemImage: "checkmark.square.fill")
+                .font(.caption)
+                .foregroundStyle(.secondary)
         }
         
-        Section {
-            Button("Sensor Calibration", systemImage: "lines.measurement.horizontal") {
-                showSensorCalibration = true
-            }
-            .sheet(isPresented: $showSensorCalibration, content: {
-                if let viewModel = environment.sensorCalibrationViewModel {
-                    NavigationStack {
-                        SensorCalibrationScreen()
-                            .environmentObject(viewModel)
-                    }
-                    .setAccent(.white)
-                }
-            })
-            .accentColor(.nordicBlue)
-            .centered()
+        Button("Sensor Calibration", systemImage: "lines.measurement.horizontal") {
+            showSensorCalibration = true
         }
+        .foregroundStyle(Color.universalAccentColor)
+        .sheet(isPresented: $showSensorCalibration, content: {
+            if let viewModel = environment.sensorCalibrationViewModel {
+                NavigationStack {
+                    SensorCalibrationScreen()
+                        .environmentObject(viewModel)
+                }
+                .setAccent(.white)
+            }
+        })
+        .centered()
     }
 }
