afb339924664fb17ef8f38b6d157ae3edb5ad1b4
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-01-15 14:34:24 +0100

    Add custom search component.

--- nRF-Toolbox/Screens/Logs/LogsPreviewScreen.swift
+++ nRF-Toolbox/Screens/Logs/LogsPreviewScreen.swift
@@ -8,11 +8,13 @@
 
 import SwiftData
 import SwiftUI
+import iOS_Common_Libraries
 
 struct LogsPreviewScreen: View {
     
     @Query(sort: \LogDb.timestamp) var logs: [LogDb]
     @State private var searchText: String = ""
+    @FocusState private var isFocused: Bool
     
     var filteredLogs: [LogDb] {
         if searchText.isEmpty {
@@ -25,13 +27,31 @@ struct LogsPreviewScreen: View {
     }
 
     var body: some View {
-        List {
-            ForEach(filteredLogs) { log in
-                Text(log.displayString)
-                    .foregroundColor(log.levelColor)
+        VStack {
+            ZStack(alignment: .leading) {
+                
+                BlinkingCursorView().hidden(!searchText.isEmpty)
+
+                HStack(spacing: 0) {
+                    TextField("Search logs", text: $searchText, prompt: Text("Search logs")).focused($isFocused).tint(.clear)
+                    BlinkingCursorView().padding(.leading, 6).hidden()
+                }
+                
+                HStack(spacing: 0) {
+                    Text(searchText).lineLimit(1).hidden()
+                    BlinkingCursorView().padding(.leading, 2).hidden(searchText.isEmpty)
+                }
             }
+            .padding()
+            
+            List {
+                ForEach(filteredLogs) { log in
+                    Text(log.displayString)
+                        .foregroundColor(log.levelColor)
+                }
+            }
+            .searchable(text: $searchText)
         }
-        .searchable(text: $searchText)
     }
 }
 
