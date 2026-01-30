43dd4b380d6198534c48d3f17082c9d5d2987068
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2026-01-30 13:41:25 +0100

    UART - improve logging.

--- nRF-Toolbox/Screens/Services/CGMS/CGMSWarningOctet.swift
+++ nRF-Toolbox/Screens/Services/CGMS/CGMSWarningOctet.swift
@@ -11,7 +11,7 @@ import iOS_Common_Libraries
 
 // MARK: - CGMSWarningOctet
 
-enum CGMSWarningOctet: RegisterValue, Option {
+enum CGMSWarningOctet: RegisterValue, Option, CustomStringConvertible {
     case resultLowerThanThePatientLowLevel,
          resulthigherThanThePatientHightLevel,
          resultLowerThanTheHypoLevel,
@@ -20,4 +20,25 @@ enum CGMSWarningOctet: RegisterValue, Option {
          rateOfIncreaseExceeded,
          resultLowerThanTheDeviceCanProcess,
          resultHigherThanTheDeviceCanProcess
+    
+    public var description: String {
+        switch self {
+        case .resultLowerThanThePatientLowLevel:
+            return "Result lower than the patient low level"
+        case .resulthigherThanThePatientHightLevel:
+            return "Result higher than the patient high level"
+        case .resultLowerThanTheHypoLevel:
+            return "Result lower than the hypo level"
+        case .resultHigherThanTheHyperLevel:
+            return "Result higher than the hyper level"
+        case .rateOfDecreaseExceeded:
+            return "Rate of decrease exceeded"
+        case .rateOfIncreaseExceeded:
+            return "Rate of increase exceeded"
+        case .resultLowerThanTheDeviceCanProcess:
+            return "Result lower thant the device can process"
+        case .resultHigherThanTheDeviceCanProcess:
+            return "Result higher thant the device can process"
+        }
+    }
 }
